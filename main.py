6a8ae1afd4b1380f5737b4841a6187a81ab650b5
fcmdhyd <69862604+fcmdhyd@users.noreply.github.com>
2021-12-31 09:30:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from art import logo
+print(logo)
+
+def add(n1,n2):
+  return n1+n2
+def substract(n1,n2):
+  return n1 - n2
+def multiply(n1,n2):
+  return n1 * n2
+def divide(n1,n2):
+  return n1/n2
+
+operations ={
+"+":add,
+"-":substract,
+"*":multiply,
+"/":divide
+}
+
+def calculator():
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation form the line above: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'yes' to continue calculating with {answer}, or type 'no' to reset.:") == 'yes':
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
